17a0145b5b052246cb9fb665fae299ce966af335
송은채 <chaeding22@gmail.com>
2025-05-26 22:51:43 +0900

    first commit

---
+++ src/main/java/com/example/ourPick/OurPickApplication.java
@@ -0,0 +1,13 @@
+package com.example.ourPick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurPickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ourPick/OurPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ourPick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
