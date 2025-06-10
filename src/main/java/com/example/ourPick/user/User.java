33ea5f3bccccec96878bf64f5b6710131a5afddf
송은채 <chaeding22@gmail.com>
2025-06-11 07:20:23 +0900

    entity

--- src/main/java/com/example/ourPick/user/User.java
+++ src/main/java/com/example/ourPick/user/User.java
@@ -1,4 +1,25 @@
 package com.example.ourPick.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+// JPA가 관리하는 엔터티(데이터베이스 테이블과 매핑되는 클래스)
+@Entity
 public class User {
+    @Id
+    // 기본 키를 자동 생성
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String authority;
+    private String identifier;
+    private String provider;
+    private String password;
+    private String accessToken;
+    private LocalDate createdAt;
+
 }
