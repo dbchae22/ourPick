3d5f2bd978b7b6c739236904881f5ebda5a565d9
송은채 <chaeding22@gmail.com>
2025-11-05 20:00:14 +0900

    ActiveProfiles import 추가

--- src/test/java/com/example/ourPick/OurPickApplicationTests.java
+++ src/test/java/com/example/ourPick/OurPickApplicationTests.java
@@ -2,13 +2,14 @@ package com.example.ourPick;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@ActiveProfiles("test") 
+@ActiveProfiles("test")
 class OurPickApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
