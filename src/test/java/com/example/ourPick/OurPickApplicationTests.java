80e5ce8752122607156cc82100d4669f41951eae
mentorrrr <minkubernetes@gmail.com>
2025-11-05 21:22:19 +0900

    Fix tc

--- src/test/java/com/example/ourPick/OurPickApplicationTests.java
+++ src/test/java/com/example/ourPick/OurPickApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@ActiveProfiles("test")
 class OurPickApplicationTests {
 
   @Test
