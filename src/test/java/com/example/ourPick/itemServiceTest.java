b91df754320db5c4326f28c66066865ad9df3f51
송은채 <chaeding22@gmail.com>
2025-11-22 18:25:18 +0900

    - ItemServiceTest로 저장/조회 테스트 통합 - @DisplayName 추가

--- src/test/java/com/example/ourPick/itemServiceTest.java
+++ src/test/java/com/example/ourPick/itemServiceTest.java
@@ -1,16 +1,19 @@
 package com.example.ourPick;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.example.ourPick.domain.Item;
+import com.example.ourPick.dto.ItemRequestRequest;
 import com.example.ourPick.dto.ItemResponse;
 import com.example.ourPick.repository.ItemRepository;
 import com.example.ourPick.service.ItemService;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,7 +21,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class itemSearchTest {
+public class itemServiceTest {
 
   @Mock
   private ItemRepository itemRepository;
@@ -27,7 +30,25 @@ public class itemSearchTest {
   private ItemService itemService;
 
   @Test
-  void 상품목록을_조회한다() {
+  @DisplayName("상품을_등록한다")
+  void regItem_ShouldCallSave() {
+    ItemRequestRequest itemRequest = ItemRequestRequest.builder()
+        .itemName("테스트 상품")
+        .storeName("테스트 매장")
+        .discountRate(10)
+        .price(10000)
+        .mainPhoto("test.jpg")
+        .createdAt("2025-11-19")
+        .build();
+
+    itemService.regItem(itemRequest);
+
+    verify(itemRepository).save(any());
+  }
+
+  @Test
+  @DisplayName("상품목록을_조회한다")
+  void getItems_ShouldCallFindAll() {
     Item item1 = new Item("itemName1", "storeName1", 10, 10000, "mainPhoto1");
     Item item2 = new Item("itemName2", "storeName2", 20, 20000, "mainPhoto2");
     List<Item> itemList = Arrays.asList(item1, item2);
@@ -42,4 +63,5 @@ public class itemSearchTest {
 
     verify(itemRepository, times(1)).findAll();
   }
+
 }
